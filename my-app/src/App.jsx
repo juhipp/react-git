9467f045d7751a3b0010b5c279b7da5dce352611
juhipp <isabohipp@gmx.de>
2025-06-16 09:41:44 +0200

    tailwind import

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RedButton from './redButton.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div class="flex">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -26,9 +27,9 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+          <RedButton />
+      </div>
     </>
   )
 }
